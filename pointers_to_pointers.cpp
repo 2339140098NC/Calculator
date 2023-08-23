db2d00c6eeb85e4b5f1a7e1bbd9a329dc5e16029
Nico <nealc58@outlook.com>
2023-08-23 00:40:19 -0700

    pointers_to_pointers.cpp

---
+++ pointers_to_pointers.cpp
@@ -0,0 +1,12 @@
+// To store te address of a pointer to integer, we need a pointer to pointer
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x = 5;
+    int *p;
+    p = &x;
+    *p = 6;
+    int **q; //this is the 
+}
